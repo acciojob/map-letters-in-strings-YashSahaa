c95810829c17e04b638736e3db2cb36fdd5bc593
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-04-12 20:54:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 //your JS code here. If required.
 function mapLetters(str) {
-
+	let obj={}
+	for(let i=0;i<str.length;i++){
+		let t = str[i]
+		if(obj[t]==undefined){
+			obj[t]=[i]
+		}
+		else{
+			obj[t].push(i)
+		}
+	}
 	
+	return obj
 }
 
 
-// const str = prompt("Enter str: ");
+ const str = prompt("Enter str: ");
 alert(JSON.stringify(mapLetters(str)));
